e545ad318a09aa13210fa17ea96c16bff2275242
DaisyRdrguez <daisy.rod19@gmail.com>
2022-03-06 19:17:11 -0600

    Added plants mapping and set up start of the db

--- src/main/java/com/revature/proj1/App.java
+++ src/main/java/com/revature/proj1/App.java
@@ -9,9 +9,37 @@ import org.apache.catalina.startup.Tomcat;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        //Database
+        Connection connection = DriverManager.getConnection("jdbc:h2:mem:test;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE;INIT=runscript from 'classpath:schema.sql'", "sa", "");
+        ResultSet rs = connection.prepareStatement("select * from plants").executeQuery();
+        List<String> plants = new ArrayList<>();
+        while(rs.next()) {
+            plants.add(rs.getString("Name"));
+        }
+
+        HttpServlet plantServlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+                    throws ServletException, IOException {
+                // JSON Mapper
+                ObjectMapper mapper = new ObjectMapper();
+                String results = mapper.writeValueAsString(plants);
+                resp.setContentType("application/json");
+                resp.getWriter().println(results);
+            }
+        };
+
         //Server
         Tomcat server = new Tomcat();
         server.getConnector();
@@ -28,6 +56,7 @@ public class App {
 
             }
         }).addMapping("/*");
+        server.addServlet("", "plantsServlet", plantServlet).addMapping("/plants");
         try {
             server.start();
         } catch (LifecycleException e) {
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table "plants" (
+    "plantsId" int primary key not null,
+    "name" varchar,
+    "price" int
+);
+
+-- create index "plantsId" on "plants" ("plantsId");
